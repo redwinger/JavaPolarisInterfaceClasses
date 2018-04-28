f1d16c4a5e80d39f4fce76833836f48ec53f7909
Matt Bacher <mattjb1991@sbcglobal.net>
2018-04-28 12:12:13 -0400

    Program works great

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Dog dog = new Dog("Hank", "Bark Bark");
+        Horse horse = new Horse("Seabiscuit", "Neigh Neigh");
+
+        System.out.println(dog.name());
+        System.out.println(dog.soundAnimalMakes());
+
+        System.out.println("***********************");
+
+        System.out.println(horse.name());
+        System.out.println(horse.soundAnimalMakes());
     }
 }
